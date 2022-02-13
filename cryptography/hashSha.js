40048e79c78ade7057420c2f8230f133a7190f02
mpilopillz <mpilo.dlamini@yahoo.com>
2022-02-13 19:26:03 +0200

    adding why not to use hash

--- cryptography/hashSha.js
+++ cryptography/hashSha.js
@@ -15,5 +15,6 @@ const match = hash1 === hash2;
 
 console.log("hash1-->", password);
 console.log("hash1-->", hash1);
+console.log("HAsh does not work becuase it returns the same hashed value the hole time so there is a database will salt values for commmon passwords");
 
 console.log(match ? "✅ good password" : '❌ password does not match');
