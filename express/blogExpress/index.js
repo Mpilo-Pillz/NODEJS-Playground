842e0604efe0d87762078dbdfb90aa4c79151745
mpilopillz <mpilo.dlamini@yahoo.com>
2022-02-15 20:25:44 +0200

    [arsing urlencoded data

--- express/blogExpress/index.js
+++ express/blogExpress/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
 });
 
 app.post('/tacos', (req, res) => {
-    res.send("POST /tacos response")
+    console.log("req.body", req.body);
+    res.send(`you sent a requet with this body: ${req.body}`)
 })
 
 app.listen(4000, () => {
-    console.log("Sercing on port 4000");
+    console.log("Serving on port 4000");
 })
