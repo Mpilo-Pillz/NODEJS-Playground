66bb30733054147b7f6614e44f33dd3334b355a4
mpilopillz <mpilo.dlamini@yahoo.com>
2022-02-16 19:50:22 +0200

    deciphering ciphered message

--- cryptography/symmetric-encrypt.js
+++ cryptography/symmetric-encrypt.js
@@ -8,3 +8,10 @@ const cipher = createCipheriv('aes256', key, iv);
 
 // Encrypt
 const encryptedMessage = cipher.update(message, 'utf8', 'hex') + cipher.final('hex');
+
+// Decrtypt
+const decipher = createDecipheriv('aes256', key, iv);
+const decryptedMessage = decipher.update(encryptedMessage, 'hex', 'utf8') + decipher.final('utf8');
+
+console.log(`Encrypted Message: ${encryptedMessage}`);
+console.log(`Decrypted Message: ${decryptedMessage}`);
