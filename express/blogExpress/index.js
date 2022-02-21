07f8dcfd255b20d326f298b8228a804c8f044b8c
mpilopillz <mpilo.dlamini@yahoo.com>
2022-02-21 06:26:17 +0200

    adding delete route

--- express/blogExpress/index.js
+++ express/blogExpress/index.js
@@ -11,7 +11,8 @@ app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
-const comments = [
+// was const before then we chenaged to let cos of filter in app.delete
+let comments = [
     {
         id: "1",
         username: "Thapelo",
@@ -73,6 +74,16 @@ app.get('/comments/:id/edit', (req, res) => {
     const comment = comments.find(c => c.id === id);
     console.log("ICEMAN-->", comment);
     res.render('comments/edit', { comment });
+})
+
+/**
+ * END
+ */
+
+app.delete('/comments/:id', (req, res) => {
+    const {id} = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
 })
 
 app.get('/tacos', (req, res) => {
