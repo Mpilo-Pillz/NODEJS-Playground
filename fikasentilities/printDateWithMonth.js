2dd4855396a71d425c16d8c21df2996e0a944ee0
mpilopillz <mpilo.dlamini@yahoo.com>
2022-03-02 00:09:42 +0200

    making more readable

--- fikasentilities/printDateWithMonth.js
+++ fikasentilities/printDateWithMonth.js
@@ -9,7 +9,9 @@ let today = new Date().toLocaleDateString()
 
 const dateArray = today.split('/')
 console.log(dateArray);
-console.log(dateArray);
+// [ '01', '03', '2022' ]
+console.log(dateArray[1].split('')[1] - 1);
+const mapDateIndexToMonthNames = dateArray[1].split('')[1] - 1
 
-const todayWithMonthName = `${dateArray[0]} ${monthNames[dateArray[1].split()[1]]} ${dateArray[2]}`
+const todayWithMonthName = `${dateArray[0]} ${monthNames[dateArray[1].split('')[1] - 1]} ${dateArray[2]}`
 console.log(todayWithMonthName);
