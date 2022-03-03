f6f279ebbfbd7ba27b51582cf594603acc62d9c0
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-03 19:51:23 +0200

    adding products page

--- express/expressCrudWithMongoose/index.js
+++ express/expressCrudWithMongoose/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("CONNECTION OPEN!!!");
+    }).catch(err => {
+        console.error("FAILED TO CONNECT", err);
+    });
 
 
 app.set('views', path.join(__dirname, 'views'));
@@ -8,8 +17,9 @@ app.set('view engine', 'ejs');
 
 const port = 4000;
 
-app.get('/dog', (req, res) => {
-    res.send("woof")
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render("products/index", { products })
 })
 app.listen(port, () => {
     console.log(` is listening on port ${port}`);
