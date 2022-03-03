3e89f01b72dd590777abbc175fc07b90d0a01c69
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-03 19:33:59 +0200

    seeding more products

--- express/expressCrudWithMongoose/seeds.js
+++ express/expressCrudWithMongoose/seeds.js
@@ -8,15 +8,46 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
         console.error("FAILED TO CONNECT", err);
     });
 
-const p = new Product({
-    name: 'Ruby Grapefruit',
-    price: 11.99,
-    category: 'fruit'
-})
+// const p = new Product({
+//     name: 'Ruby Grapefruit',
+//     price: 11.99,
+//     category: 'fruit'
+// })
 
-p.save().then(p => {
-    console.log(p)
-})
-    .catch(e => {
-        console.log(e);
-    })
+// p.save().then(p => {
+//     console.log(p)
+// })
+//     .catch(e => {
+//         console.log(e);
+//     })
+
+const seedProducts = [
+    {
+        name: 'Watermelon',
+        price: 50.50,
+        category: 'fruit'
+    },
+    {
+        name: 'Goat Milk',
+        price: 8.99,
+        category: 'dairy'
+    },
+    {
+        name: 'Cheese',
+        price: 23.45,
+        category: 'dairy'
+    },
+    {
+        name: 'Lettuce',
+        price: 6.71,
+        category: 'vegetable'
+    },
+    {
+        name: 'Spinach',
+        price: 3.48,
+        category: 'vegetable'
+    },
+
+];
+
+Product.insertMany(seedProducts).then(res => console.log(res)).catch(e => console.log(e))
