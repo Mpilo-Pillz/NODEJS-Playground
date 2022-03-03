43e0d45bf76e4f5a6aa2c3f44bddf9ef3dc9cb32
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-03 19:34:58 +0200

    setting up express server

--- express/expressCrudWithMongoose/index.js
+++ express/expressCrudWithMongoose/index.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("CONNECTION OPEN!!!");
-    }).catch(err => {
-        console.error("FAILED TO CONNECT", err);
-    })
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
