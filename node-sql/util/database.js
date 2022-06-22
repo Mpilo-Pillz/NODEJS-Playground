8dc21f64a56fdfe92a9b9bf66118c146afb8ba19
mpilopillz <mpilo.dlamini@yahoo.com>
2022-06-22 05:06:38 +0200

    moving db file

--- node-sql/util/database.js
+++ node-sql/util/database.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
+console.log("envy->", process.env);
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "node-complete",
-  password: process.env.DB_PASSWORD,
+  password: process.env.MY_SQL_DB_PASSWORD,
 });
+
+module.exports = pool.promise();
