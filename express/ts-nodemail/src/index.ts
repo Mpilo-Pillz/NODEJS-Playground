801eafbeef9de25885076d6cd2e90a8df9c1502c
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-07-31 20:32:24 +0200

    serving the aplication

--- express/ts-nodemail/src/index.ts
+++ express/ts-nodemail/src/index.ts
@@ -1,1 +1,14 @@
-console.log("heleb");
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 4300;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`<div>
+    <h1>Server online</h1>
+    </div>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
