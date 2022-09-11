9c3b123427e85b954e8e847c6f03c31a5b56403a
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-09-11 19:09:32 +0200

    not showing db details on log

--- express/ts-nodemail/src/config/db.ts
+++ express/ts-nodemail/src/config/db.ts
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.DATABASE_URL ?? "");
-    console.log(`MongoDB Connected: ${connect.connection.host}`);
+    console.log(`MongoDB Connected: ${connect.connection.readyState}`);
   } catch (err) {
-    console.error(`Failed to Connect to DB`);
+    console.error(`Failed to Connect to DB: ${err}`);
     process.exit(1);
   }
 };
