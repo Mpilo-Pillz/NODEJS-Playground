20aa379e5c7f01a4c66371185954815f62327d00
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-11-24 02:09:32 +0200

    adding email as arg

--- express/ts-nodemail/src/customer-portal/utils/generateToken.ts
+++ express/ts-nodemail/src/customer-portal/utils/generateToken.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const generateToken = (id: string) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET as string, {
+const generateToken = (id: string, email: string) => {
+  return jwt.sign({ id, email }, process.env.JWT_SECRET as string, {
     expiresIn: "1h",
   });
 };
