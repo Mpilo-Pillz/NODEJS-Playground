615a24f61354cf2caa0db86d6eb3ac7999592ae7
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-10-07 13:42:45 +0200

    connecting to multiple dbs

--- express/ts-nodemail/src/config/db.ts
+++ express/ts-nodemail/src/config/db.ts
@@ -1,13 +1,59 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+export const customerPortalConnect = mongoose.createConnection();
+export const cvConnect = mongoose.createConnection();
+
+export const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.DATABASE_URL ?? "");
-    console.log(`MongoDB Connected: ${connect.connection.readyState}`);
+    await customerPortalConnect.openUri(process.env.DATABASE_URL ?? "");
+    await cvConnect.openUri(process.env.CV_DATABASE_URL ?? "");
+    console.log(`MongoDB Connected: ${customerPortalConnect.readyState}`);
+    console.log(`MongoDB Connected: ${cvConnect.readyState}`);
   } catch (err) {
     console.error(`Failed to Connect to DB: ${err}`);
     process.exit(1);
   }
 };
 
-export default connectDB;
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     const customerPortalConnect = await mongoose.createConnection(
+//       process.env.DATABASE_URL ?? ""
+//     );
+//     const cvConnect = await mongoose.createConnection(
+//       process.env.CV_DATABASE_URL ?? ""
+//     );
+//     console.log(`MongoDB Connected: ${customerPortalConnect.readyState}`);
+//     console.log(`MongoDB Connected: ${cvConnect.readyState}`);
+
+//     return { customerPortalConnect, cvConnect }; // return the connections
+//   } catch (err) {
+//     console.error(`Failed to Connect to DB: ${err}`);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     const customerPortalConnect = await mongoose.connect(
+//       process.env.DATABASE_URL ?? ""
+//     );
+//     const cvConnect = await mongoose.connect(process.env.CV_DATABASE_URL ?? "");
+
+//     console.log(`MongoDB Connected: ${customerPortalConnect.connection.readyState}`);
+//     console.log(`MongoDB Connected: ${cvConnect.connection.readyState}`);
+
+//     return {cvConnect, customerPortalConnect}
+//   } catch (err) {
+//     console.error(`Failed to Connect to DB: ${err}`);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
